0d23aaffe968df804cdd18210c7a22ed85f73463
setgen <StevenEtgen@wifi-roaming-128-4-157-176.host.udel.edu>
2019-10-02 15:57:25 -0400

    More database setup and connecting with local mongodb

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
+const { body, validationResult } = require('express-validator/check');
+
 router.get('/', (req, res) => {
-  res.render('form');
+  res.render('form', { title: 'Registration form' });
 });
+
+router.post('/',
+  [
+    body('name')
+      .isLength({ min: 1 })
+      .withMessage('Please enter a name'),
+    body('email')
+      .isLength({ min: 1 })
+      .withMessage('Please enter an email'),
+  ], 
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      res.send('Thank you for your registration!');
+    } else {
+      res.render('form', {
+        title: 'Registration form',
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+  }
+);
 
 module.exports = router;
