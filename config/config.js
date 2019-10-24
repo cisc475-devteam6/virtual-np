9be6d9c698bef31119baeecbdbfebf04daea3be8
Joel Turk <jturk@udel.edu>
2019-10-24 19:07:26 -0400

    remove outer files

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    //Secret key for JWT signing and encryption
-    'secret': 'a3C($M@f<2p0',
-    //Database connection information
-    'database': 'mongodb+srv://setgen:rpp9wULhT0IJHEcI@virtual-np-la4q4.mongodb.net/user-profiles?retryWrites=true&w=majority',
-    //Setting port for server
-    'port': process.env.PORT || 3000
-}
