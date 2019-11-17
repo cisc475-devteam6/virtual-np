05783e2f3285934953296171855f187feb5c1cc9
ariegner <ARiegner18@gmail.com>
2019-11-17 13:22:06 -0500

    working on http requests to access symptoms api

--- frontend/src/app/services/symptomsAPI/symptoms-api.service.ts
+++ frontend/src/app/services/symptomsAPI/symptoms-api.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+//var unirest = require("unirest");
 
 @Injectable({
   providedIn: 'root'
 })
 export class SymptomsAPIService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  public getSymptoms(): string[] {
-    return ["Headache", "Stomachache", "Rash"];
+  /*
+  var req = unirest("GET", "https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms");
+    req.query({
+      "format":"json",
+      "language":"en-gb"
+    });
+    req.headers({
+      "x-rapidapi-host":"priaid-symptom-checker-v1.p.rapidapi.com",
+      "x-rapidapi-key":"SIGN-UP-FOR-KEY"
+    });
+    req.end(function(res) {
+      if (res.error) throw new Error(res.error);
+      symptoms = res.body;
+    })
+    return symptoms;
+  */
+
+  public getSymptoms() {
+    let symptoms: string[];
+
+    let result = this.http.get<string[]>("priaid-symptom-checker-v1.p.rapidapi.com/symptoms/");
+    console.log(result);
+    return symptoms;
   }
 
 }
