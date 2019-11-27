1e177f296571a067070c65fdb48ee3b6aedc7398
ariegner <ARiegner18@gmail.com>
2019-11-27 01:13:47 -0500

    added some fixes

--- frontend/src/app/services/symptomsAPI/symptoms-api.service.ts
+++ frontend/src/app/services/symptomsAPI/symptoms-api.service.ts
@@ -9,48 +9,18 @@ export class SymptomsAPIService {
 
   private headers = new HttpHeaders({
     "x-rapidapi-host": "priaid-symptom-checker-v1.p.rapidapi.com",
-		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+		"x-rapidapi-key": "39005a4e53mshd68d48c5674370dp12ad63jsnf044c0e1708d"
   });
 
   constructor(private http: HttpClient) { }
 
-  /*
-  var req = unirest("GET", "https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms");
-    req.query({
-      "format":"json",
-      "language":"en-gb"
-    });
-    req.headers({
-      "x-rapidapi-host":"priaid-symptom-checker-v1.p.rapidapi.com",
-      "x-rapidapi-key":"SIGN-UP-FOR-KEY"
-    });
-    req.end(function(res) {
-      if (res.error) throw new Error(res.error);
-      symptoms = res.body;
-    })
-    return symptoms;
-  */
-
-  
-  /*
   public getSymptoms() {
-    let symptoms: string[];
-
-    let result = this.http.get<string[]>("priaid-symptom-checker-v1.p.rapidapi.com/symptoms/").pipe(map(data => {
-      console.log(data);
-    })).subscribe(result =>{
-      console.log(result);
-    });
-    console.log(result);
-    return symptoms;
-  }
-  */
-
-  public getSymptoms() {
-    return this.http.get<any>("priaid-symptom-checker-v1.p.rapidapi.com/symptoms/", {headers:this.headers}).toPromise().then(response => {
+    let names: string[];
+    this.http.get<any>("https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms/", {headers:this.headers}).toPromise().then(response => {
       let data = response;
       console.log(data);
-      return data;
+      names.push(data);
     });
+    return names;
   }
 }
