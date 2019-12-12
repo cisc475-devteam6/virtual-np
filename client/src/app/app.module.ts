93d117c59ea974495f1480d80e0790aefa8a404a
Connor Onweller <onweller@udel.edu>
2019-12-11 19:10:25 -0500

    Organization

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { ErrorInterceptor } from './security/interceptors/error.interceptor'
-// import { JwtInterceptor } from './security/interceptors/jwt.interceptor'
-// import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
-import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
-import { SecurityModule } from './security/security.module';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { UserChatPageComponent } from './pages/user-chat-page/user-chat-page.component';
-import { AdminChatPageComponent } from './pages/admin-chat-page/admin-chat-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { SecurityModule } from './security/security.module';
 
-import { HeaderComponent } from './navigation/components/header/header.component'
-import { VisitPageComponent } from './pages/visit-page/visit-page.component';
+import { AppComponent } from './app.component';
+import {
+  HomePageComponent,
+  SignInPageComponent,
+  SignUpPageComponent,
+  LandingPageComponent,
+  UserChatPageComponent,
+  AdminChatPageComponent,
+  VisitPageComponent
+} from './pages';
+
+
+import { HeaderComponent } from './components/navigation/header/header.component'
 import { DropdownSelectComponent } from './components/dropdown-select/dropdown-select.component';
 
 
@@ -47,8 +47,6 @@ import { DropdownSelectComponent } from './components/dropdown-select/dropdown-s
   ],
   providers: [
     {provide: DropdownSelectComponent, useClass: DropdownSelectComponent}
-        // { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-        // { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
   bootstrap: [AppComponent]
 })
